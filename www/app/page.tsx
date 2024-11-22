6f174ddb5349b195b7a01a92f848511818a24fa3
adii1203 <aditya32ft@gmail.com>
2024-11-22 22:17:01 +0530

    Add chart download funcnality and local fonts

--- www/app/page.tsx
+++ www/app/page.tsx
@@ -2,6 +2,7 @@
 
 import { Input } from "@/components/ui/input";
 import Image from "next/image";
+import * as htmlToImage from "html-to-image";
 
 import {
   LineChart,
@@ -12,6 +13,9 @@ import {
   CartesianGrid,
   ResponsiveContainer,
 } from "recharts";
+import { Button } from "@/components/ui/button";
+import { useRef } from "react";
+import { Download } from "lucide-react";
 
 const data = [
   {
@@ -77,6 +81,29 @@ const data = [
 ];
 
 export default function Home() {
+  const divRef = useRef<HTMLDivElement>(null);
+  const exportAsImage = async (el: HTMLDivElement | null, fileName: string) => {
+    if (!el) return;
+    const blob = await htmlToImage.toPng(el);
+    downloadImage({ blob, fileName });
+  };
+
+  const downloadImage = ({
+    blob,
+    fileName,
+  }: {
+    blob: string;
+    fileName: string;
+  }) => {
+    const a = document.createElement("a");
+    a.download = `${fileName}_gitgraph.png`;
+    a.href = blob;
+    document.body.appendChild(a);
+    a.click();
+    document.body.removeChild(a);
+    a.remove();
+  };
+
   return (
     <div>
       <div>
@@ -94,35 +121,37 @@ export default function Home() {
           </div>
         </div>
       </div>
-      <div className="bg-white space-y-6 font-SpaceGrotesk mx-auto mt-10 p-6 rounded-lg shadow-2xl w-[100%] sm:w-[600px]">
-        <div className="flex items-stretch space-y-0 border-b p-0">
-          <div className="flex flex-1 flex-col justify-center gap-1 px-2 py-4">
-            <div className="flex items-center gap-2">
-              <div className="w-8 h-8 rounded-full overflow-hidden">
-                <Image
-                  src="https://avatars.githubusercontent.com/u/153106492?s=200&v=4"
-                  alt="repo image"
-                  width={40}
-                  height={40}
-                />
+      <div className="relative">
+        <div className="p-4 relative" ref={divRef}>
+          <div className="bg-white space-y-6 mx-auto mt-10 p-6 rounded-lg shadow-2xl w-[100%] sm:w-[600px]">
+            <div className="flex items-stretch space-y-0 border-b p-0">
+              <div className="flex flex-1 flex-col justify-center gap-1 px-2 py-4">
+                <div className="flex items-center gap-2">
+                  <div className="w-8 h-8 rounded-full overflow-hidden">
+                    <Image
+                      src="https://avatars.githubusercontent.com/u/153106492?s=200&v=4"
+                      alt="repo image"
+                      width={40}
+                      height={40}
+                    />
+                  </div>
+                  <h3 className="text-2xl font-semibold leading-none tracking-tight">
+                    dubinc/dub
+                  </h3>
+                </div>
               </div>
-              <h3 className="text-2xl font-semibold leading-none tracking-tight">
-                dubinc/dub
-              </h3>
+              <div className="flex border-l">
+                <div className="flex flex-1 flex-col justify-center gap-1 px-6 py-4 text-left sm:px-8 sm:py-4">
+                  <span className="text-xs">Total Stars</span>
+                  <span className="text-lg font-bold">19,000</span>
+                </div>
+              </div>
             </div>
-          </div>
-          <div className="flex border-l">
-            <div className="flex flex-1 flex-col justify-center gap-1 px-6 py-4 text-left sm:px-8 sm:py-4">
-              <span className="text-xs">Total Stars</span>
-              <span className="text-lg font-bold">19,000</span>
-            </div>
-          </div>
-        </div>
-        <div className="">
-          <div
-            className="flex
+            <div className="">
+              <div
+                className="flex
       w-full
-        font-SpaceGrotesk
+      
         justify-center
         text-xs
       [&_.recharts-cartesian-axis-tick_text]:fill-gray-400
@@ -136,30 +165,49 @@ export default function Home() {
         [&_.recharts-sector[stroke='#fff']]:stroke-transparent 
         [&_.recharts-sector]:outline-none 
         [&_.recharts-surface]:outline-none">
-            <ResponsiveContainer width="100%" height={300}>
-              <LineChart data={data}>
-                <XAxis
-                  minTickGap={20}
-                  padding={{ left: 10, right: 40 }}
-                  axisLine={false}
-                  tickLine={false}
-                  dataKey="date"
-                />
-                <YAxis axisLine={false} tickLine={false} dataKey={"stars"} />
-                <Tooltip />
-                <Line
-                  type={"natural"}
-                  legendType="line"
-                  dot={false}
-                  strokeWidth={2}
-                  strokeLinecap="round"
-                  dataKey={"stars"}
-                  stroke="#ffc073"
-                />
-                <CartesianGrid vertical={false} stroke="#ccc" opacity={0.3} />
-              </LineChart>
-            </ResponsiveContainer>
+                <ResponsiveContainer width="100%" height={300}>
+                  <LineChart data={data}>
+                    <XAxis
+                      minTickGap={20}
+                      padding={{ left: 10, right: 40 }}
+                      axisLine={false}
+                      tickLine={false}
+                      dataKey="date"
+                    />
+                    <YAxis
+                      axisLine={false}
+                      tickLine={false}
+                      dataKey={"stars"}
+                    />
+                    <Tooltip />
+                    <Line
+                      type={"natural"}
+                      legendType="line"
+                      dot={false}
+                      strokeWidth={2}
+                      strokeLinecap="round"
+                      dataKey={"stars"}
+                      stroke="#ffc073"
+                    />
+                    <CartesianGrid
+                      vertical={false}
+                      stroke="#ccc"
+                      opacity={0.3}
+                    />
+                  </LineChart>
+                </ResponsiveContainer>
+              </div>
+            </div>
           </div>
+        </div>
+        <div>
+          <Button
+            variant={"ghost"}
+            size={"icon"}
+            className="rounded-full absolute right-10 top-16"
+            onClick={() => exportAsImage(divRef.current, "dub")}>
+            <Download size={16} />
+          </Button>
         </div>
       </div>
     </div>
